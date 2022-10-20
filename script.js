e8ce60b9a1afd1dc5b3360c5ad29039adc1b61cc
Alisha S <sorianoalisha@gmail.com>
2022-10-20 17:00:45 -0400

    Write the logic for the 'compute' function, and create another function to update the rate span text dynamically

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    const principal = document.getElementById("principal").value;
+    if(principal <= 0) {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();  //will highlight the 'principal' input box once the user clicks 'OK' on the alert
+    }
+
+
+    const rate = document.getElementById('rate').value;
+    const years = document.getElementById('num_of_years').value;
+    const interest = principal * years * rate / 100;
+
+    const year = new Date().getFullYear()+parseInt(years);
+    const result = document.getElementById('result');
+
+    result.innerHTML = `If you deposit <span class='highlight'>${principal}</span>,
+        at an interest rate of <span class='highlight'>${rate}</span>.
+        You will receive an amount of <span class='highlight'>${interest}</span>,
+        in the year <span class='highlight'>${year}</span>`;
 }
         
+function updateRate() {
+    const rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = `${rateval}%`;
+}
